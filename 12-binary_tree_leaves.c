9b202ff9b90422644e77b9a108e3cda45aeddad2
Ailwei <Ailweimaemu@gmail.com>
2024-01-04 08:50:29 -0800

    12. Leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -21,8 +21,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		return (1);
 	}
 	left_leave = binary_tree_leaves(tree->left);
-        right_leave = binary_tree_leaves(tree->right);
-
+	right_leave = binary_tree_leaves(tree->right);
 	return (left_leave + right_leave);
-
 }
