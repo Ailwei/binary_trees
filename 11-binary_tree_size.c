5317ebe1e23e7f73702bae180254d58b19d2f966
Ailwei <Ailweimaemu@gmail.com>
2024-01-04 08:43:05 -0800

    11. Size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -16,11 +16,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	
-		left_size = binary_tree_size(tree->left);
-		right_size = binary_tree_size(tree->right);
-
-
-		return left_size + right_size + 1;
-
-	}
+	left_size = binary_tree_size(tree->left);
+	right_size = binary_tree_size(tree->right);
+	return (left_size + right_size + 1);
+}
